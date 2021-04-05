e98bff763ab2c078507f169b9af82287e306141b
Kaliya Baibosunova <k.baibosunova@gmail.com>
2021-04-05 23:43:11 +0300

    finish first two stages

---
+++ Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -0,0 +1,18 @@
+package tictactoe;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String s = scanner.nextLine();
+        System.out.println("---------");
+        System.out.println("| " + s.charAt(0) + " " +
+                s.charAt(1) + " " + s.charAt(2) + " |");
+        System.out.println("| " + s.charAt(3) + " " +
+                s.charAt(4) + " " + s.charAt(5) + " |");
+        System.out.println("| " + s.charAt(6) + " " +
+                s.charAt(7) + " " + s.charAt(8) + " |");
+        System.out.println("---------");
+    }
+}
